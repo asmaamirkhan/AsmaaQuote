855be446d9744a61259739b22cc1afcce5d894ff
Asmaa Mirkhan <45575570+asmaamirkhan@users.noreply.github.com>
2019-09-21 16:22:08 +0300

    Basics 👩‍🔧

---
+++ src/components/Main.js
@@ -0,0 +1,44 @@
+// author: Asmaa Mirkhan ~ 2019
+import React, { Component } from 'react';
+
+import 'antd/dist/antd.css';
+import '../styles/style.css';
+import { Layout, Menu } from 'antd';
+
+import { Typography } from 'antd';
+import Logo from './media/logo.png'
+
+const { Title } = Typography;
+const { Header, Content, Footer } = Layout;
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+      
+      <img alt='image' src={Logo} style = {{height: '40px'}}/>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['1']}
+        style={{ lineHeight: '64px', float: "right" }}
+      >
+        <Menu.Item key="1">Main</Menu.Item>
+        <Menu.Item key="2">Other</Menu.Item>
+        <Menu.Item key="3">About</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '50px 50px', marginTop: 64 }}>
+      
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>AsmaaQuote Project ©2019 Created by Asmaa Mirkhan</Footer>
+  </Layout>
+      </div>
+    );
+  }
+}
+
+export default Main;
---
+++ src/components/Quote.js
@@ -0,0 +1,15 @@
+// author: Asmaa Mirkhan ~ 2019
+import React, { Component } from 'react';
+
+
+class Quote extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Quote;
