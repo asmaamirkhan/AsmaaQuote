6f6910631d7b991feaeb837d9cd7a65f4a903b4c
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2019-12-04 15:44:07 +0300

    Updates

---
+++ precache-manifest.53cc69dcb798e23a4ce12d3950cc6569.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb33f66e5d3d354b2220bcbdf0c466d",
+    "url": "/AsmaaQuote/index.html"
+  },
+  {
+    "revision": "5180ad70dc7a29cce52b",
+    "url": "/AsmaaQuote/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "87c2381203dc5cca9083",
+    "url": "/AsmaaQuote/static/css/main.ac6c194e.chunk.css"
+  },
+  {
+    "revision": "5180ad70dc7a29cce52b",
+    "url": "/AsmaaQuote/static/js/2.148c2659.chunk.js"
+  },
+  {
+    "revision": "87c2381203dc5cca9083",
+    "url": "/AsmaaQuote/static/js/main.3dbb2068.chunk.js"
+  },
+  {
+    "revision": "a3bcfe6470b9ce679572",
+    "url": "/AsmaaQuote/static/js/runtime-main.9e596a6e.js"
+  },
+  {
+    "revision": "ddc725ed2ffe5adf3549e1b2af79bcc4",
+    "url": "/AsmaaQuote/static/media/logo.ddc725ed.png"
+  }
+]);
