9ee2aee329dad0b3f30f01042fac630908c2bdfe
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-01-11 12:13:58 +0300

    Petty fixes 👩‍🔧

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 import AsmaaQuoteMain from './components/AsmaaQuoteMain'
 function App() {
   return (
     
-      <div className="App">
+      <div>
          <AsmaaQuoteMain/>
       </div>
     
