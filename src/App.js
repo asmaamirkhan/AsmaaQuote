0df93301fdf205a6755273937c390bc0c16fb4a5
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-01-11 09:13:31 +0300

    Revolution 🔥

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-/*
- author: Asmaa Mirkhan ~ 2019
-*/
-import { Route, Switch, BrowserRouter, HashRouter } from 'react-router-dom';
-
 import React from 'react';
-import Main from './components/Main';
-import About from './components/About';
-import Categories from './components/data/categories'; 
-function App() {
+import logo from './logo.svg';
+import './App.css';
 
+function App() {
   return (
-          <Switch>
-            {
-              Categories.map((item, key) => {
-                let path = '/'+item.title;
-                return (
-                  <Route exact path={path} component={Main} />
-                )
-              })
-            }
-          <Route exact path="/About" component={About} />
-          <Route path="/" component={Main} />
-          <Route path="/asmaaquote" component={Main} />
-          </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
