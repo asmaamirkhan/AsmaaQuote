cea3c06279d505a5d9210287d438f12fe0d88149
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-01-12 09:32:07 +0300

    About page is done ✔️

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+/* 
+  author: Asmaa Mirkhan ~ 2019
+*/
+
 import React from "react";
-import AsmaaQuoteMain from "./components/AsmaaQuoteMain";
 import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Categories from "./components/media/categories";
-import dotenv from 'dotenv';
+import AsmaaQuoteMain from "./components/AsmaaQuoteMain";
+import AsmaaQuoteAbout from "./components/AsmaaQuoteAbout";
+
 function App() {
-  dotenv.config()
   return (
     <Router>
       <Switch>
@@ -22,12 +26,17 @@ function App() {
         <Route
           path="/About"
           component={() => (
-            <AsmaaQuoteMain category={Categories[0]} selected={Categories.length+1} />
+            <AsmaaQuoteAbout
+              category={Categories.length + 1}
+              selected={Categories.length + 1}
+            />
           )}
         />
         <Route
           path="/"
-          component={() => <AsmaaQuoteMain category={Categories[0]} selected={0} />}
+          component={() => (
+            <AsmaaQuoteMain category={Categories[0]} selected={0} />
+          )}
         />
       </Switch>
     </Router>
--- src/components/AsmaaQuoteMain.js
+++ src/components/AsmaaQuoteMain.js
@@ -3,13 +3,12 @@
 */
 
 import React, { Component } from 'react';
-import AsmaaQuoteMenu from './AsmaaQuoteMenu';
-import AsmaaQuoteContent from './AsmaaQuoteContent';
-import AsmaaQuoteFooter from './AsmaaQuoteFooter';
 import { Layout } from 'antd';
 import 'antd/dist/antd.css';
 
-const { Footer } = Layout;
+import AsmaaQuoteMenu from './AsmaaQuoteMenu';
+import AsmaaQuoteContent from './AsmaaQuoteContent';
+import AsmaaQuoteFooter from './AsmaaQuoteFooter';
 
 class AsmaaQuoteMain extends Component {
   constructor(props) {
@@ -22,7 +21,7 @@ class AsmaaQuoteMain extends Component {
         <Layout className="layout">
           <AsmaaQuoteMenu selected={this.props.selected} />
           <AsmaaQuoteContent category={this.props.category} />
-          
+          <AsmaaQuoteFooter/>
         </Layout>
       </div>
     );
