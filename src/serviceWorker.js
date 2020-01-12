f81d0ccbf892cce25687946544e716c5215c242a
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-01-12 14:14:26 +0300

    Cleaning 💦

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -33,48 +33,6 @@ export function register(config) {
 
     window.addEventListener("load", () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
-      /*
-       * PWA Settings START
-       */
-      var deferredPrompt;
-
-      function addToHomeScreen() {
-        var a2hsBtn = document.querySelector(".ad2hs-prompt"); // hide our user interface that shows our A2HS button
-        a2hsBtn.style.display = "none"; // Show the prompt
-        deferredPrompt.prompt(); // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then(function(choiceResult) {
-          if (choiceResult.outcome === "accepted") {
-            console.log("User accepted the A2HS prompt");
-          } else {
-            console.log("User dismissed the A2HS prompt");
-          }
-
-          deferredPrompt = null;
-        });
-      }
-
-      function showAddToHomeScreen() {
-        var a2hsBtn = document.querySelector(".ad2hs-prompt");
-
-        a2hsBtn.style.display = "block";
-
-        a2hsBtn.addEventListener("click", addToHomeScreen);
-      }
-
-      window.addEventListener("beforeinstallprompt", function(e) {
-        // Prevent Chrome 67 and earlier from automatically showing the prompt
-        e.preventDefault();
-        // Stash the event so it can be triggered later.
-        deferredPrompt = e;
-
-        showAddToHomeScreen();
-      });
-
-      /*
-       * PWA Settings END
-       */
-      
       
        if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
