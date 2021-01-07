147a6efdcb2ecea17c3605937f40ea97b311f364
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2021-01-07 22:10:04 +0300

    Renewed about section ✨

--- src/components/AsmaaQuoteAbout.js
+++ src/components/AsmaaQuoteAbout.js
@@ -1,10 +1,11 @@
 /* 
-  author: Asmaa Mirkhan ~ 2019
+  author: Asmaa Mirkhan ~ 2021
+  TODO: move strings to firebase and make the structure dynamic 🙄
 */
 
 import React, { Component } from "react";
 import { Layout, Row, Col, BackTop, Card } from "antd";
-import { FaArrowCircleUp, FaCode } from "react-icons/fa";
+import { FaArrowCircleUp, FaCode, FaMagic, FaPodcast, FaCertificate } from "react-icons/fa";
 import { GiButterfly } from "react-icons/gi";
 import ReactMarkdown from "react-markdown";
 
@@ -99,7 +100,7 @@ class AsmaaQuoteAbout extends Component {
                 <Card
                   title={AboutStrings.AboutAsmaaQuestion}
                   bordered={true}
-                  style={{ background: "#ffe4f1", border: "2px solid #ffcce5" }}
+                  style={{ background: "#ffe6d1", border: "2px solid #ffcfab" }}
                 >
                   <p>
                     <ReactMarkdown source={AboutStrings.AboutAsmaa} />
@@ -107,7 +108,223 @@ class AsmaaQuoteAbout extends Component {
                 </Card>
               </Col>
             </Row>
+            <Row gutter={12} type="flex">
+              <Col
+                xs={24}
+                sm={6}
+                md={6}
+                lg={6}
+                xl={6}
+                style={{ marginTop: 16, display: "flex" }}
+              >
+                <FaMagic
+                  size="6em"
+                  color="#FFFFFF"
+                  style={{
+                    border: "2px solid #560027",
+                    borderRadius: "50px",
+                    padding: "20px",
+                    background: "#560027",
+                    margin: "auto"
+                  }}
+                />
+              </Col>
 
+              <Col
+                xs={24}
+                sm={18}
+                md={18}
+                lg={18}
+                xl={18}
+                style={{ marginTop: 16 }}
+              >
+                <Card
+                  title={AboutStrings.AboutAsmaaQuestionPersonal}
+                  bordered={true}
+                  style={{ background: "#ffe4f1", border: "2px solid #ffcce5" }}
+                >
+                  <p>
+                    <ReactMarkdown
+                      source={AboutStrings.AboutAsmaaPersonal}
+                    />
+                  </p>
+                </Card>
+              </Col>
+            </Row>
+
+
+            <Row gutter={12} type="flex">
+              <Col
+                xs={24}
+                sm={6}
+                md={6}
+                lg={6}
+                xl={6}
+                style={{ marginTop: 16, display: "flex" }}
+              >
+                <FaCode
+                  size="6em"
+                  color="#FFFFFF"
+                  style={{
+                    border: "2px solid #131E66",
+                    borderRadius: "50px",
+                    padding: "20px",
+                    background: "#131E66",
+                    margin: "auto"
+                  }}
+                />
+              </Col>
+
+              <Col
+                xs={24}
+                sm={18}
+                md={18}
+                lg={18}
+                xl={18}
+                style={{ marginTop: 16 }}
+              >
+                <Card
+                  title={AboutStrings.AboutAsmaaQuestionTech}
+                  bordered={true}
+                  style={{ background: "#e2f1ff", border: "2px solid #cde9ff" }}
+                >
+                  <p>
+                    <ReactMarkdown
+                      source={AboutStrings.AboutAsmaaTech}
+                    />
+                  </p>
+                </Card>
+              </Col>
+            </Row>
+            <Row gutter={12} type="flex">
+              <Col
+                xs={24}
+                sm={6}
+                md={6}
+                lg={6}
+                xl={6}
+                style={{ marginTop: 16, display: "flex" }}
+              >
+                <FaPodcast
+                  size="6em"
+                  color="#FFFFFF"
+                  style={{
+                    border: "2px solid #bfbfbf",
+                    borderRadius: "50px",
+                    padding: "20px",
+                    background: "#bfbfbf",
+                    margin: "auto"
+                  }}
+                />
+              </Col>
+
+              <Col
+                xs={24}
+                sm={18}
+                md={18}
+                lg={18}
+                xl={18}
+                style={{ marginTop: 16 }}
+              >
+                <Card
+                  title={AboutStrings.AboutAsmaaQuestionImportance}
+                  bordered={true}
+                  style={{ background: "#f3eafa", border: "2px solid #e1dfdd" }}
+                >
+                  <p>
+                    <ReactMarkdown
+                      source={AboutStrings.AboutAsmaaImportance}
+                    />
+                  </p>
+                </Card>
+              </Col>
+            </Row>
+            <Row gutter={12} type="flex">
+              <Col
+                xs={24}
+                sm={6}
+                md={6}
+                lg={6}
+                xl={6}
+                style={{ marginTop: 16, display: "flex" }}
+              >
+                <FaCertificate
+                  size="6em"
+                  color="#FFFFFF"
+                  style={{
+                    border: "2px solid #560027",
+                    borderRadius: "50px",
+                    padding: "20px",
+                    background: "#560027",
+                    margin: "auto"
+                  }}
+                />
+              </Col>
+
+              <Col
+                xs={24}
+                sm={18}
+                md={18}
+                lg={18}
+                xl={18}
+                style={{ marginTop: 16 }}
+              >
+                <Card
+                  title={AboutStrings.AboutAsmaaQuestionGallery}
+                  bordered={true}
+                  style={{ background: "#ffe4f1", border: "#ffcce5" }}
+                >
+                  <p>
+                    <ReactMarkdown
+                      source={AboutStrings.AboutAsmaaGallery}
+                    />
+                  </p>
+                </Card>
+              </Col>
+            </Row>
+            <Row gutter={12} type="flex">
+              <Col
+                xs={24}
+                sm={6}
+                md={6}
+                lg={6}
+                xl={6}
+                style={{ marginTop: 16, display: "flex" }}
+              >
+                <GiButterfly
+                  size="6em"
+                  color="#FFFFFF"
+                  style={{
+                    border: "2px solid #FF9C08",
+                    borderRadius: "50px",
+                    padding: "20px",
+                    background: "#FF9C08",
+                    margin: "auto"
+                  }}
+                />
+              </Col>
+
+              <Col
+                xs={24}
+                sm={18}
+                md={18}
+                lg={18}
+                xl={18}
+                style={{ marginTop: 16 }}
+              >
+                <Card
+                  title={AboutStrings.AboutAsmaaQuestionButterflies}
+                  bordered={true}
+                  style={{ background: "#ffe6d1", border: "2px solid #ffcfab" }}
+                >
+                  <p>
+                    <ReactMarkdown
+                      source={AboutStrings.AboutAsmaaButterflies}
+                    />
+                  </p>
+                </Card>
+              </Col>
+            </Row>
             <Row gutter={12} type="flex">
               <Col
                 xs={24}
@@ -151,6 +368,7 @@ class AsmaaQuoteAbout extends Component {
                 </Card>
               </Col>
             </Row>
+
           </Content>
           <AsmaaQuoteFooter />
         </Layout>
--- src/components/media/AsmaaQuoteAboutStrings.js
+++ src/components/media/AsmaaQuoteAboutStrings.js
@@ -1,13 +1,64 @@
+/* 
+  author: Asmaa Mirkhan ~ 2020
+  TODO: move strings to firebase and make the structure dynamic 🙄
+*/
 const AboutStrings = {
-  AboutProjectQuestion: "What is AsmaaQuote? 👼",
+  AboutProjectQuestion: `What is AsmaaQuote? `,
   AboutProject:
-    "**AsmaaQuote** is the quote manager of [Asmaa 🦋](https://www.linkedin.com/in/asmaamirkhan/) where she shares quotes that she likes and writes. quotes at AsmaaQuote will be updated as Asmaa writes or learns new quotes 👩‍🏫. **STAY TUNED** 🎉",
+    `**AsmaaQuote** is the quote manager of [Asmaa 🦋](https://www.linkedin.com/in/asmaamirkhan/) where she shares 
+    quotes that she likes and writes. quotes at AsmaaQuote will be updated as Asmaa writes or learns new quotes 👩‍🏫. 
+    **STAY TUNED** 🎉 `,
   AboutAsmaaQuestion: "Who is Asmaa? 🦋",
   AboutAsmaa:
-    " As a **life position** I am just a learner 🤔 who wants to learn continuously, no matter about what, I want to increase my knowledge and awareness by every useful information. At the same time I like to write my thoughts and due to that I created this small platform. For my educational and professional info check my [LinkedIn profile 🙋‍♀️](https://www.linkedin.com/in/asmaamirkhan/).",
+    `I am Asmaa (if you are Turkish it is okay to call me Esma 😊), a Computer Engineering last year student at
+         Istanbul University (and lifelong learner 🧐) 
+  * 🤔 As a life position I am just a learner who wants to learn continuously, no matter about what, I just want to increase my knowledge and awareness by every useful information. 
+  * 📝 At the same time I like to write my thoughts and to save quotes that I like. Due to that I created this platform.  `,
+  AboutAsmaaQuestionPersonal: "Want to know more personally about me? 😇",
+  AboutAsmaaPersonal:
+    `
+  * 🦋 In love with challenges and butterflies
+  * 💦 Can fit in any cup like water
+  * 🍕 Hungry for knowledge and have passion in learning new things continuously
+  * 🤓 Developing my self as a HUMAN is my top priority believing that making the world a better place to live starts from individuals
+  * 🌺 Searching about beauty by inspecting details believing that it is reserved in them`,
+  AboutAsmaaQuestionTech: "Want to know more technically about me? 👩‍💻",
+  AboutAsmaaTech:
+    `
+  * 💻 I have an experience in Web and Android development that I gained from internships and school projects I have done
+  * 💫 I am working on artificial intelligence, machine learning and computer vision for now (very hard work 🤓)
+  * 🧐 Specifically, I am focusing on implementing AI empowered projects on the edge
+        `,
+  AboutAsmaaQuestionImportance: "Why is it important to share quotes? 🧐",
+  AboutAsmaaImportance:
+    `Nowadays we are trying to develop ourselves in terms of career and it is really an important thing ⭐, but beside that we 
+          have to always remember that we are humans and we have to be kind in every situation 🧐, we have to develop ourselves
+          in terms of humanity too 💖! So that, I decided to make MAIN domain only for quotes that aim developing human
+          values, awareness and kindness 🥰. I will do my best to make it more and more inspirational 🦋.
+          `,
+  AboutAsmaaQuestionGallery: "What about gallery section? 📷",
+  AboutAsmaaGallery:
+    `Oh, I forgot to tell you that I love nature and nature photography 🌳, nature really rests our brains 🧠, and to make 
+    these photos more inspirational I add quotes to photos and share them on Instagram as stories 🎈. I really love that 🥰, 
+    so most of these stories are now under gallery tab 🎉, check them out and give your brain a rest! 🎁 
+    `,
+  AboutAsmaaQuestionButterflies: "Why do I love butterflies? 🥰",
+  AboutAsmaaButterflies:
+    `A butterfly was a caterpillar 🐛, that caterpillar realized its ugly sides and decided to turn into that sweet creature 🎀, into
+             a butterfly 🦋! and to add beauty to every place it goes to 🦸‍♀️! 
+            This proess really inpires me a lot 💫. Realizing our ugly sides and making an effort to beautify them and to add beauty, kindness 
+            and softness to every place we go to are really important and pricesless things 🎁, and they really worth 💎. Maybe making the 
+            world a better place to live starts in this way ✨! Who knows?! 😉 
+            So, let's be butterflies, let's spread beauty! 🤗
+            `,
   AboutProjectTechnicalQuestion: "What is AsmaaQuote? (I am a programmer 👩‍💻)",
   AboutProjectTechnical:
-    "**AsmaaQuote** is an open-source 💖✨ quote manager made by [Asmaa](https://github.com/asmaamirkhan) using: \n* ⚛ ReactJS \n* 🐜 AntDesign \n* 🔥 Firebase \n* You can configure **AsmaaQuote** to create your own quote manager easily just by editing few lines of code 🤗\n    * Documentation is under development, it will be puplished on GitHub as it will be ready, wait for it! ⏰ \n * Check out GitHub repo [here](https://github.com/asmaamirkhan/AsmaaQuote)🔍."
+    `**AsmaaQuote** is an open-source 💖✨ quote manager made by [Asmaa](https://github.com/asmaamirkhan) using: 
+  * ⚛ ReactJS 
+  * 🐜 AntDesign 
+  * 🔥 Firebase 
+
+Check out GitHub repo [here](https://github.com/asmaamirkhan/AsmaaQuote)🔍. `
 };
 
 export default AboutStrings;
